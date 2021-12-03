bd341107c2f366f7ec1e64642a46aade21586ba2
jordan315 <95484722+jordan315@users.noreply.github.com>
2021-12-03 08:50:55 -0600

    Update header.js

--- code/pages/components/header.js
+++ code/pages/components/header.js
@@ -3,5 +3,100 @@ document.write('<p class="activity">hello</p>');
 /*
 <div id="banner">
 
+////
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+
+body { 
+  margin: 0;
+}
+
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 30px 5px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.header a {
+  color: black;
+  text-align: center;
+  padding: 20px;
+  text-decoration: none;
+  font-size: 20px; 
+  line-height:40px;
+  border-radius: 4px;
+
+
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+
+.header-r {
+  float: right;
+}
+
+.header-l {
+  float: left;
+}
+
+.header-m {
+  float: middle;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .header-right {
+    float: none;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <a class="logo">Nav Bar</a>
+  <a class="logo">Logo</a>
+  <div class="header-r">
+    <a href="#about">Settings</a>
+  </div>
+</div>
+
+<div style="padding-left:20px; background-color: rgb(80,80,80); font-size: 50px; padding-top:40px">
+  <h1>Responsive Header</h1>
+  <p>Resize the browser window to see the effect.</p>
+  <p>Some content..</p>
+  <p>Some content..</p>
+  <p>Some content..</p>
+  <p>Some content..</p>
+  <p>Some content..</p>
+  <p>Some content..</p>
+</div>
+
+</body>
+</html>
+////
+
 </div>
 */
