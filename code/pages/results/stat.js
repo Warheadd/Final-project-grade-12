64487ba82e3be374920e7d9d163f64f9200c1f91
jordan315 <95484722+jordan315@users.noreply.github.com>
2022-01-12 09:13:53 -0500

    Update stat.js

--- code/pages/results/stat.js
+++ code/pages/results/stat.js
@@ -3,15 +3,15 @@ const global = require("../../globals.js");
 const checkHistory = global.checkHistory;
 const checkDay = global.checkDay;
 
-// creates a function to compare the user's statistics with averages
-
-   
-// Reads the data for the current day
+// compare the user's statistics with averages
+// Reads the data regarding the amount of grams of carbon that the user preoduces on a certain day
 var carbonData;
+
+// tries to read the file
 try {
-    
     carbonData = readFileSync("faris.json", "utf8");
 } 
+// catches the errors if they are any
 catch (err) {
     console.error(err);
 }
@@ -23,6 +23,7 @@ if(!check(carbonData)) {
 // If it is correctly formatted, it parses the json file
 carbonData = JSON.parse(carbonData);  
     
+// creates an object named recommended with properties
 var recommended = {
 "advice":{
     "transportation":true,
@@ -36,32 +37,41 @@ var recommended = {
 "average":1000
 }
     
-//all numbers are in grams
-    
+// all numbers are in grams
+// compares user's carbon emission from transportation with the average
 if(carbonData.transportation >2485.5) {
   recommended.advice[transportation] = true;
 }
+// compares user's carbon emission from food with the average
 if(carbonData.food > 5620) {
    recommended.advice[food] = true;
 }
+// compares user's carbon emission from lights with the average
 if(carbonData.lights >195) {
    recommended.advice[led] = true;
 }
+// compares user's carbon emission from screens with the average
 if(carbonData.screen >2.6) {
    recommended.advice[screens] = true;
 }
+// compares user's carbon emission from purchases with the average
 if(carbonData.purchases >42140) {
    recommended.advice[purchase] = true;
 } 
+// compares user's carbon emission from heating with the average
 if(carbonData.heating >7787) {
    recommended.advice[heating] = true;
 }
 
+// creates a variable and sets it to 1
 var earthsRequired = 1;
 
+// compares the user's total carbon produced (grams) in a single day and divides it by how much Earth can absorb per day
+// this is to find out how many Earths would be required to sustain a world where everybody created as much carbon as you did 
 earthsRequired = carbonData.total / 9942;
 
-earths = earthsRequired.toFixed(2);
+// takes the number of Earths required to sustain your world and rounds it to two decimal places
+recommended.earths = earthsRequired.toFixed(2);
        
     
 
