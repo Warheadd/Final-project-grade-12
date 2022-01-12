f1649f742f629b64d79fb8df306b017b123b641e
jordan315 <95484722+jordan315@users.noreply.github.com>
2022-01-12 00:31:03 -0500

    Update stat.js

--- code/pages/results/stat.js
+++ code/pages/results/stat.js
@@ -4,36 +4,53 @@ const checkHistory = global.checkHistory;
 const checkDay = global.checkDay;
 
 // creates a function to compare the user's statistics with averages
-function compare() {
+
    
-    // Reads the data for the current day
-    var carbonData;
-    try {
-        carbonData = readFileSync("data/today.json", "utf8");
-    } 
-    catch (err) {
-        console.error(err);
+// Reads the data for the current day
+var carbonData;
+try {
+    
+    carbonData = readFileSync("faris.json", "utf8");
+} 
+catch (err) {
+    console.error(err);
+}
+
+// Checks if the json is correctly formatted, if not it redirects to an error
+if(!check(carbonData)) {  
+    window.location.href = "../error/historyError.html";
+}
+// If it is correctly formatted, it parses the json file
+carbonData = JSON.parse(carbonData);  
+    
+var recommended = {
+"advice":{
+    "transportation":true,
+    "food":true,
+    "led":true,
+    "screens":true,
+    "purchase":true,
+    "heating":true,
+    },
+"earths":1,
+"average":1000
+}
+    
+    //all numbers are in grams
+    
+    if(carbonData.transportation >2485.5) {
+      recommended.advice[transportation] = true;
+    }
+    if(carbonData.food > 5620) {
+       recommended.advice[food] = true;
+    }
+    if(carbonData.lights >195) {
+       recommended.advice[led] = true;
     }
 
-    // Checks if the json is correctly formatted, if not it redirects to an error
-    if(!check(carbonData)) {  
-        window.location.href = "../error/historyError.html";
-    }
-    // If it is correctly formatted, it parses the json file
-    carbonData = JSON.parse(carbonData);  
+    if(carbonData.purchases >42140) {
+       recommended.advice[purchase] = true;
     
-    var recommended = {
-    "advice":{
-        "transportation":true,
-        "food":true,
-        "led":true,
-        "screens":true,
-        "purchase":true,
-        "heating":true,
-        },
-    "earths":5.6,
-    "average":1000
-    }
- 
-    if(carbonData.transportation[0] >1) return true;
-}
+
+    
+
