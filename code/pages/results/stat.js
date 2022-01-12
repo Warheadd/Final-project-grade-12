a5dce860c34ff8a5dd70a1036d6f5a5f04d70096
jordan315 <95484722+jordan315@users.noreply.github.com>
2022-01-11 20:57:08 -0500

    Update stat.js

--- code/pages/results/stat.js
+++ code/pages/results/stat.js
@@ -1,18 +1,24 @@
 const {readFileSync, writeFile} = require("fs");
-const global = require("../../../globals.js");
+const global = require("../../globals.js");
+const checkHistory = global.checkHistory;
+const checkDay = global.checkDay;
 
-// Reads the data for the current day
-var todayData;
-try {
-    todayData = readFileSync("pages/history/editingDay.json", "utf8");
-} 
-catch (err) {
-    console.error(err);
-}
+// creates a function to compare the user's statistics with averages
+function compare() {
 
-// Checks if the json is correctly formatted, if not it redirects to an error
-if(!check(todayData)) {  
-    window.location.href = "../error/historyError.html";
+    // Reads the data for the current day
+    var carbonData;
+    try {
+        carbonData = readFileSync("data/today.json", "utf8");
+    } 
+    catch (err) {
+        console.error(err);
+    }
+
+    // Checks if the json is correctly formatted, if not it redirects to an error
+    if(!check(carbonData)) {  
+        window.location.href = "../error/historyError.html";
+    }
+    // If it is correctly formatted, it parses the json file
+    carbonData = JSON.parse(carbonData);  
 }
-// If it is correctly formatted, it parses the json file
-todayData = JSON.parse(todayData);  
