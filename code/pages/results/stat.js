a1de720a9f5eb43a5935dbe807b64dfc0fb9cc2e
jordan315 <95484722+jordan315@users.noreply.github.com>
2022-01-11 19:18:54 -0500

    Update stat.js

--- code/pages/results/stat.js
+++ code/pages/results/stat.js
@@ -1,1 +1,18 @@
-const
+const {readFileSync, writeFile} = require("fs");
+const global = require("../../../globals.js");
+
+// Reads the data for the current day
+var todayData;
+try {
+    todayData = readFileSync("pages/history/editingDay.json", "utf8");
+} 
+catch (err) {
+    console.error(err);
+}
+
+// Checks if the json is correctly formatted, if not it redirects to an error
+if(!check(todayData)) {  
+    window.location.href = "../error/historyError.html";
+}
+// If it is correctly formatted, it parses the json file
+todayData = JSON.parse(todayData);  
